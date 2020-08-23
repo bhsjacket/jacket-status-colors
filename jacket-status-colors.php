f3aa5d0db6df6d8091e887f5f2f37d74f204ba4d
Jerome Paulos <jeromepaulos@yahoo.com>
2020-08-22 17:45:50 -0700

    Initial commit

--- jacket-status-colors.php
+++ jacket-status-colors.php
@@ -5,3 +5,10 @@ Plugin URI: https://github.com/bhsjacket/jacket-status-colors
 Author: Jerome Paulos
 Author URI: https://jeromepaulos.com
 */
+
+function jacket_registerStatusCSS($hook) {
+    if( $hook === 'edit.php' && get_post_type() === 'post' ) {
+        wp_enqueue_style( 'jacket-status-colors', plugin_dir_url(__FILE__) . 'jacket-status-colors.css' );
+    }
+}
+add_action( 'admin_enqueue_scripts', 'jacket_registerStatusCSS' );
